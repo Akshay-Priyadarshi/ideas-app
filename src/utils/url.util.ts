bd481da02701bda43eae1921ec9e13324b59531a
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-02-03 20:40:42 +0530

    fixed redirect url

--- src/utils/url.util.ts
+++ src/utils/url.util.ts
@@ -11,7 +11,7 @@ export const getBaseUrl = (req: Request) => {
 	const isDev = process.env.NODE_ENV === 'development'
 	const PORT = getEnv('PORT')
 	const hostname = isDev ? `${req.hostname}:${PORT}` : `${req.hostname}`
-	const protocol = req.secure ? 'https' : 'http'
+	const protocol = isDev ? 'http' : 'https'
 	const baseUrl = `${protocol}://${hostname}`
 	return baseUrl
 }
