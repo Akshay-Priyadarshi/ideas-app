67c89010d638c8890605b324d14c99df16a26847
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-05-13 18:29:43 +0530

    fixed title case error

--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,4 +1,7 @@
-export function titleCase(s: string) {
+export function titleCase(s: string | undefined) {
+    if (!s) {
+        return;
+    }
     const formatted = s[0].toUpperCase() + s.slice(1);
     return formatted;
 }
